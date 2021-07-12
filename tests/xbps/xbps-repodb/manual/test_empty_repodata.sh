633f221b3c5c43f9347f4811dbaf2ff0d7ffba96
Piotr Wójcik <chocimier@tlen.pl>
2021-07-12 17:33:12 +0200

    xbps-repodb: extend manual tests from x86_64 to all archs

--- tests/xbps/xbps-repodb/manual/test_empty_repodata.sh
+++ tests/xbps/xbps-repodb/manual/test_empty_repodata.sh
@@ -3,7 +3,32 @@
 DESTDIR="${DESTDIR:-/}"
 
 cd $(dirname ${0})
-rm -r repos
-./fetch_to_stage.sh
-LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-repodb" -i -d repos/*
+WORKDIR="$(mktemp -d)"
+cp repos-2021-07-06.tar.xz "${WORKDIR}"
+cd "$WORKDIR"
+tar xf repos-2021-07-06.tar.xz
+for i in repos/*; do
+	echo $i
+	arch="$(echo ${i%-repodata} | rev | cut -d/ -f1 | cut -d_ -f1 | rev)"
+	case $arch in
+		64*) arch="x86_${arch%-repodata}"
+	esac
+	[ -z "$arch" ] && continue
+	mkdir -p $arch
+	ln -sr $i $arch
+	echo $arch-stagedata
+	(cd $i; tar -zcvf $arch-stagedata index.plist index-meta.plist)
+	echo
+done
+
+rm repos-2021-07-06.tar.xz
+for arch in *; do
+	[ "$arch" = repos ] && continue
+	echo
+	echo repodb $arch
+	env XBPS_ARCH=$arch LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-repodb" -i $arch/*
+done
+cd ..
+rm -r "$WORKDIR"
+
 
--- tests/xbps/xbps-repodb/manual/test_update_repodata.sh
+++ tests/xbps/xbps-repodb/manual/test_update_repodata.sh
@@ -2,21 +2,50 @@
 
 DESTDIR="${DESTDIR:-/}"
 
-cd "$(dirname ${0})"
-rm -r repos
-./fetch_to_stage.sh # writes stagedata
-tar xf repos-2020-12-27.tar.xz # writes plist
-for i in repos/*x86_64-repodata; do
-	(cd $i &&
-		pwd
-		mv x86_64-stagedata x86_64-repodata
-		LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-checkvers" -D $(xdistdir) -e --format='%n-%r.x86_64.xbps' -i --repository=$PWD |
-			xargs env LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-rindex" -R
-		cat ../../outdated |
-			xargs env LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-rindex" -R
-		mv x86_64-repodata x86_64-stagedata
-		tar -zcf x86_64-repodata index.plist index-meta.plist
-	)
-done
-LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-repodb" -i -d repos/*x86_64-repodata
+cd $(dirname ${0})
+WORKDIR="$(mktemp -d)"
+cp repos-2021-07-06.tar.xz "${WORKDIR}"
+cp repos-2020-12-27.tar.xz "${WORKDIR}"
+cp outdated-* "${WORKDIR}"
+cd "$WORKDIR"
 
+compress_index() {
+	tarball="$1"
+	stage="$2"
+	find -name '*.plist' -delete
+	tar xf "$tarball"
+	for i in repos/*; do
+		echo $i
+		[ -e "$i/index.plist" ] || continue
+		arch="$(echo ${i%-repodata} | rev | cut -d/ -f1 | cut -d_ -f1 | rev)"
+		case $arch in
+			64*) arch="x86_${arch%-repodata}"
+		esac
+		mkdir -p $arch
+		[ -e $arch/$(basename $i) ] || ln -sr $i $arch
+		(
+			cd $i;
+			tar -zcvf $arch-repodata index.plist index-meta.plist
+			if [ "$stage" ]; then
+				XBPS_ARCH=$arch LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-checkvers" -D $(xdistdir) -e --format='%n-%r.noarch.xbps' -i --repository=$PWD |
+					xargs env XBPS_ARCH=$arch LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-rindex" -R >/dev/null
+				cat ../../outdated*$arch,* | sed -e 's/$/.'$arch'.xbps/' |
+					xargs env XBPS_ARCH=$arch LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-rindex" -R
+				mv $arch-repodata $arch-stagedata
+			fi
+		)
+		echo
+	done
+}
+
+compress_index repos-2021-07-06.tar.xz stage
+compress_index repos-2020-12-27.tar.xz
+rm repos-2021-07-06.tar.xz repos-2020-12-27.tar.xz outdated-*
+for arch in *; do
+	[ "$arch" = repos ] && continue
+	echo
+	echo repodb $arch
+	env XBPS_ARCH=$arch LD_LIBRARY_PATH="${DESTDIR}/usr/local/lib" "${DESTDIR}/usr/local/bin/xbps-repodb" -i $arch/*
+done
+cd ..
+rm -r "$WORKDIR"
